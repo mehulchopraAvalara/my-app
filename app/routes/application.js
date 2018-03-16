a8d8f6abeb84a00b136732f1f2e4c3246382da40
mehul chopra <mehul.chopra@avalara.com>
2018-03-16 15:22:58 +0530

    direct git link to library engine

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     title(tokens) {
-      debugger;
       tokens.push('::MY ENGINE APP::')
       return tokens.join(' | ');
     },
